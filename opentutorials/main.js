787c25ca3e8de350a7e87d5d26aad3bf9f9a0b13
YoungKoungKim <0koung.kim@gmail.com>
2021-01-10 17:29:48 +0900

    2021.01.10 function을 이용한 코드정리

--- opentutorials/main.js
+++ opentutorials/main.js
@@ -2,6 +2,33 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+function templateHTML(title, list, body) {
+  return `<!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            ${body}
+          </body>
+          </html>
+          `;
+}
+
+function templateList(filelist) {
+  var list = '<ul>'
+  var i = 0;
+  while (i < filelist.length) {
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</li>`
+    i = i + 1;
+  }
+  list = list + '</ul>'
+  return list;
+}
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
@@ -11,13 +38,7 @@ var app = http.createServer(function(request,response){
           fs.readdir('./data', function(error, filelist) {
             var title = 'Welcome';
             var description = 'Hello, Node.js';
-            var list = '<ul>'
-            var i = 0;
-            while (i < filelist.length) {
-              list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</li>`
-              i = i + 1;
-            }
-            list = list + '</ul>'
+            var list = templateList(filelist);
             /*
             '<ul>
               <li><a href="/?id=HTML">HTML</a></li>
@@ -25,53 +46,16 @@ var app = http.createServer(function(request,response){
               <li><a href="/?id=JavaScript">JavaScript</a></li>
             </ul>'
             */
-            var template = `
-            <!doctype html>
-            <html>
-            <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              ${list}
-              <h2>${title}</h2>
-              <p>${description}</p>
-            </body>
-            </html>
-            `;
+            var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
             response.writeHead(200);
             response.end(template);
           });
       } else {
         fs.readdir('./data', function(error, filelist) {
-          var title = 'Welcome';
-          var description = 'Hello, Node.js';
-          var list = '<ul>'
-          var i = 0;
-          while (i < filelist.length) {
-            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</li>`
-            i = i + 1;
-          }
-          list = list + '</ul>'
-
+          var list = templateList(filelist);
           fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
             var title = queryData.id;
-            var template = `
-            <!doctype html>
-            <html>
-            <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              ${list}
-              <h2>${title}</h2>
-              <p>${description}</p>
-            </body>
-            </html>
-            `;
+            var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
             response.writeHead(200);
             response.end(template);
           });
