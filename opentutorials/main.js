8640129a4034e68feab99dfdc2601f59387a84cb
YoungKoungKim <0koung.kim@gmail.com>
2021-01-28 21:27:11 +0900

    2021.01.28 글 수정 수정할 정보 전송

--- opentutorials/main.js
+++ opentutorials/main.js
@@ -70,7 +70,7 @@ var app = http.createServer(function(request,response) {
           var title = queryData.id;
           var list = templateList(filelist);
           var template = templateHTML(title, list, `
-            <form action="http://localhost:3000/create_process" method="post">
+            <form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
               <textarea name="description" placeholder="description"></textarea>
@@ -98,6 +98,27 @@ var app = http.createServer(function(request,response) {
           //302 : redirection
           response.writeHead(302, {Location: `/?id=${title}`});
           response.end();
+        });
+      });
+    } else if (pathname === '/update') {
+      fs.readdir('./data', function(error, filelist){
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+          var title = queryData.id;
+          var list = templateList(filelist);
+          var template = templateHTML(title, list, `
+            <form action="/update_process" method="post">
+            <input type="hidden" name="id" value="${title}">
+            <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+            <p>
+              <textarea name="description" placeholder="description">${description}</textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+            `, '');
+          response.writeHead(200);
+          response.end(template);
         });
       });
     } else {
