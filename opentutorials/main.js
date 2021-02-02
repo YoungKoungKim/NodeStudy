2bb8c46ab9a7cd5fa743e1170aac1d3f67276b85
YoungKoungKim <0koung.kim@gmail.com>
2021-02-02 23:47:40 +0900

    2021.02.02 글삭제 버튼 form 수정

--- opentutorials/main.js
+++ opentutorials/main.js
@@ -58,7 +58,14 @@ var app = http.createServer(function(request,response) {
           fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
             var title = queryData.id;
             var list = templateList(filelist);
-            var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`,`<a href="/create">create</a> <a href="/update?id=${title}">update</a> <a href="/delete?id=${title}">delete</a>`);
+            var template = templateHTML(title, list,
+                        `<h2>${title}</h2><p>${description}</p>`,
+                        `<a href="/create">create</a>
+                         <a href="/update?id=${title}">update</a>
+                         <form action="/delete_process" method="post" onsubmit="return confirm('삭제하시겠습니까?')">
+                            <input type="hidden" name="id" value="${title}">
+                            <input type="submit" value="delete">
+                         </form>`);
             response.writeHead(200);
             response.end(template);
           });
@@ -139,6 +146,8 @@ var app = http.createServer(function(request,response) {
         });
         //console.log(post);
       });
+    } else if (pathname === '/delete_process') {
+      
     } else {
       response.writeHead(404);
       response.end('Not found');
